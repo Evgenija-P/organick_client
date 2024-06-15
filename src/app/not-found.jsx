f54821837bc6cbda8382a273275f78187bac082c
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2024-06-15 18:24:46 +0300

    404

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,11 +6,11 @@ export default function Page() {
       <section className="w-full h-full bg-notFound bg-no-repeat flex flex-col relative">
         <div className="w-fit right-[260px] top-[150px] absolute">
           <p className="text-[#8FA8A8] text-[300px] font-black leading-none">404</p>
-          <p className="text-main text-[70px] font-extrabold text-center">Page not found</p>
+          <p className="text-main text-[70px] font-extrabold text-center">Сторінку не знайдено</p>
           <p className="text-grey text-lg mb-6">
-            The page you are looking for doesn't exist or has been moved
+            Сотрінка, яку ти шукаєш, була видалена чи переміщена
           </p>
-          <LinkButton title="Go to Homepage" goTo="/" style={"main"} page="notfound"/>
+          <LinkButton title="На домашню сторінку" goTo="/" style={"main"} page="notfound" />
         </div>
       </section>
     </main>
